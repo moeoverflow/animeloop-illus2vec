9a1a5d4c893d50484bd9f9b4fa408ff94263cb46
ShinCurry <shincurryyang@gmail.com>
2017-10-19 14:02:48 +0800

    little changes - add comment - filter .jpg/png image files - tqdm configure

--- config.example.py
+++ config.example.py
@@ -1,3 +1,5 @@
 config = {
-    images_path: ''
+    'images_path': '',
+    'caffemodel': './illustration2vec/illust2vec_tag_ver200.caffemodel',
+    'tag_list': './illustration2vec/tag_list.json'
 }
--- main.py
+++ main.py
@@ -9,22 +9,25 @@ from bson.objectid import ObjectId
 
 from config import config
 
+# Logger configure
 logging.getLogger('illus2vec')
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logger = logging.getLogger()
 
 IMAGES_PATH = config['images_path']
 
-logger.info('Loading chainer models...')
-illust2vec = i2v.make_i2v_with_chainer(
-    "./illustration2vec/illust2vec_tag_ver200.caffemodel", "./illustration2vec/tag_list.json")
-logger.info('Load completed.')
-
-logger.info('Connect to database.')
+# Database initial
+logger.info('Connecting to database...')
 client = MongoClient("localhost", 27017)
 db = client.animeloop_tags
 
+# chainer models initial
+logger.info('Loading chainer models...')
+illust2vec = i2v.make_i2v_with_chainer(
+    config['caffemodel'], config['tag_list'])
 
+
+# save tags estimated from image file into dababase
 def to_tags(filename, loopid):
     image = Image.open(filename)
     result = illust2vec.estimate_plausible_tags([image], threshold=0.5)[0]
@@ -35,7 +38,8 @@ def to_tags(filename, loopid):
         'lang': 'en'
     }
 
-    # Extract tags from database to memory.
+    # Extract tags from database to memory
+    # for performance optimization
     saved_tags = list(db.tags.find({'loopid': ObjectId(loopid)}))
 
     def exist_in_tagslist(loopid, type, value):
@@ -54,22 +58,29 @@ def to_tags(filename, loopid):
             tag['value'] = item[0]
             tag['confidence'] = item[1]
 
+            # Avoid saving duplicate data
             if not exist_in_tagslist(loopid, tag['type'], tag['value']):
                 db.tags.insert_one(tag)
 
     db.tagscheck.insert_one({'loopid': ObjectId(loopid)})
 
 
+# performance optimization
 saved_tagscheck = map(lambda tc: str(tc['loopid']), list(db.tagscheck.find({})))
 
 logger.info('Loading files list...')
 files = os.listdir(IMAGES_PATH)
 
-progress_bar = tqdm(files, 'Estimating tags', bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt}')
+logger.info('Estimating tags')
+progress_bar = tqdm(files, ascii=True, dynamic_ncols=True, bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt}')
 for file in progress_bar:
     if not os.path.isdir(file):
         filename = IMAGES_PATH + '/' + file
         loopid = os.path.splitext(file)[0]
+        ext = os.path.splitext(file)[1]
+
+        if not (ext == '.jpg' or ext == '.png'):
+            continue
 
         if loopid not in saved_tagscheck:
             to_tags(filename, loopid)
